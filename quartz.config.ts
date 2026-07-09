d4c225030cef5653b2d2a86da0240d38262351d0
bryanpetruescu <bryanpetruescu@outlook.com>
2026-07-09 08:06:35 -0700

    Quartz sync: Jul 9, 2026, 8:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "bns.petruescu.me",
+    baseUrl: "bns.auramite.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
